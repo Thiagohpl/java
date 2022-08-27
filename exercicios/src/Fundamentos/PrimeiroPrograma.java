f690f0b829c6fa984772b5c24bf49ce969bb17f7
Thiago Henrique <104402878+Thiagohpl@users.noreply.github.com>
2022-08-27 17:39:18 -0300

    AT02

--- exercicios/src/Fundamentos/PrimeiroPrograma.java
+++ exercicios/src/Fundamentos/PrimeiroPrograma.java
@@ -2,6 +2,12 @@ package Fundamentos;
 
 public class PrimeiroPrograma {
 
+/**
+ * 
+ * @Autor Thiago Henrique
+ */
+	
+	// Uma Setençade código ;
 	public static void main(String[] args) {
 		System.out.println("Primeiro Programa!");
 
